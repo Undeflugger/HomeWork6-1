ec9dd462201e2db4b032006dd8f2c85d3b47f953
Undeflugger <104163210+Undeflugger@users.noreply.github.com>
2022-12-21 19:47:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 function removeElement(array, item){
-    let index = array.indexOf(item);
+    const index = array.indexOf(item);
     if (index !== -1){
        array.splice(index, 1);
     }
